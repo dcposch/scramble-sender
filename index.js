fdef9a8b093e5fbd04cd0b9eea0e60096830558a
DC <dcposch@dcpos.ch>
2016-01-19 07:48:19 -0800

    Registration, session gen and encryption work, decryption doesn't
    Also libaxolotl-javascript is an immature and incomplete library. The first page of the README contains basic documentation errors. Issues filed.

--- index.js
+++ index.js
@@ -15,59 +15,62 @@
 // being said, but also who is talking to whom.
 var axolotl = require('axolotl')
 
+var NUM_PREKEYS = 5
+
 // Registers a new user. Produces the user's ID, public and private key, and prekey pairs.
 // Calls back with a ready-to-use axolotl instance and underlying store.
 function register (callback) {
   var identityKeyPair = null
   var registrationId = null
-  var signedPreKeyPair = []
+  var signedPreKeyPairs = []
   var preKeyPairs = []
+  var sessions = {}
   var store = {
     getLocalIdentityKeyPair: function () { return identityKeyPair },
     getLocalRegistrationId: function () { return registrationId },
     getLocalSignedPreKeyPair: function (id) { return signedPreKeyPairs[id] },
-    getLocalPreKeyPair: function (id) { return preKeyPairs[id] }
+    getLocalPreKeyPair: function (id) { return preKeyPairs[id] },
+
+    // the following are described in the libaxolotl-javascript README,
+    // but are not actually used at all. weak.
+    getRemotePreKeyBundle: function() { throw new Error('unimplemented') },
+    isRemoteIdentityTrusted: function() { throw new Error('unimplemented') },
+    hasSession: function (name) { return !!sessions[name] },
+    getSession: function (name) { return sessions[name] },
+    putSession: function(name, session) { sessions[name] = session } 
   }
+
   var axol = axolotl(store)
+
   axol.generateIdentityKeyPair().then(function (result) {
     identityKeyPair = result
     console.log('IdentityKeyPair: ' + keyPairToString(result))
-
-    axol.generateRegistrationId().then(function (result) {
-      registrationId = result
-      console.log('RegistrationId: ' + result)
-
-      axol.generateSignedPreKey(identityKeyPair, 1).then(function (result) {
-        signedPreKeyPair[result.id] = result
-        console.log('SignedKeyPair #' + result.id + ': ' + keyPairToString(result.keyPair))
-
-        axol.generatePreKeys(0, 100).then(function (result) {
-          preKeyPairs = result
-          console.log('PreKeys:\n\t' + result.map(function (pair) {
-            return 'PreKey #' + pair.id + ': ' + keyPairToString(pair.keyPair)
-          }).join('\n\t'))
-
-          axol.generateLastResortPreKey().then(function (result) {
-            preKeyPairs[result.id] = result
-            console.log('LastResortPreKey #' + result.id + ': ' + keyPairToString(result.keyPair))
-
-            callback(axol, store)
-          })
-        })
-      })
-    })
+    return axol.generateRegistrationId()
+  }).then(function (result) {
+    registrationId = result
+    console.log('RegistrationId: ' + result)
+    return axol.generatePreKeys(0, NUM_PREKEYS)
+  }).then(function (result) {
+    preKeyPairs = result
+    console.log('PreKeys:\n\t' + result.map(function (pair) {
+      return 'PreKey #' + pair.id + ': ' + keyPairToString(pair.keyPair)
+    }).join('\n\t'))
+    return axol.generateLastResortPreKey()
+  }).then(function (result) {
+    preKeyPairs[result.id] = result
+    console.log('LastResortPreKey #' + result.id + ': ' + keyPairToString(result.keyPair))
+    return axol.generateSignedPreKey(identityKeyPair, 1)
+  }).then(function (result) {
+    signedPreKeyPairs[result.id] = result
+    console.log('SignedKeyPair #' + result.id + ': ' + keyPairToString(result.keyPair) +
+     '\n\tsignature: ' + new Buffer(result.signature).toString('hex'))
+    callback(axol, store)
   })
 }
 
 function keyPairToString (keyPair) {
-  return ('(public ' + hex(keyPair.public)
-    + ', private ' + hex(keyPair.private) + ')')
-}
-
-function hex (buffer) {
-  return Array.prototype.map.call(new Uint8Array(buffer), function (x) {
-    return (0x100 + x).toString(16).substring(1)
-  }).join('')
+  return ('(public ' + new Buffer(keyPair.public).toString('hex') +
+    ', private ' + new Buffer(keyPair.private).toString('hex') + ')')
 }
 
 // Produces an encrypted, signed, base64-encoded Axlotl message containing
--- test.js
+++ test.js
@@ -12,39 +12,74 @@ test('bar', function (t) {
 })
 
 test('register', function (t) {
-  sender.register(function(axol, store) {
+  sender.register(function (axol, store) {
     console.log('Generated registration info')
     t.end()
   })
 })
 
 test('communicate', function (t) {
-  sender.register(function(axolA, storeA) {
+  sender.register(function (axolA, storeA) {
     console.log('Generated registration info A')
-    sender.register(function(axolB, storeB) {
+    sender.register(function (axolB, storeB) {
       console.log('Generated registration info B')
 
       console.log('\n\nA to B: hello world')
-      // TODO: fail happens here
-      var enc = axolA.encryptMessage('bob', toBytes('hello world')).then(function (result) {
-        console.log(result)
+      var preKeyBundleForB = {
+        identityKey: storeB.getLocalIdentityKeyPair().public,
+        preKeyId: 0,
+        preKey: storeB.getLocalPreKeyPair(0).keyPair.public,
+        signedPreKeyId: 1,
+        signedPreKey: storeB.getLocalSignedPreKeyPair(1).keyPair.public,
+        signedPreKeySignature: storeB.getLocalSignedPreKeyPair(1).signature
+      }
+      console.log(['preKeyBundleForB', preKeyBundleForB])
 
-        console.log('\n\nB to A: yolo')
-
-        console.log('\n\nA to B: swag')
-
+      console.log('Creating sessAtoB')
+      axolA.createSessionFromPreKeyBundle(preKeyBundleForB).then(function (sessAtoB) {
+        console.log('Encrypting message from A to B')
+        return axolA.encryptMessage(sessAtoB, toBytes('magic letters'))
+      }, function (err) {
+        t.end('failed to create session')
+      }).then(function (msg) {
+        console.log('Encrypted! message:' + prettyPrint(msg, 1))
+        console.log('B decrypting message')
+        return axolB.decryptPreKeyWhisperMessage(null, msg.body)
+      }, function (err) {
+        t.end('failed to encrypt')
+      }).then(function (msg) {
+        console.log('Decrypted! message:' + prettyPrint(msg, 1))
         t.end()
+      }, function (err) {
+        t.end('failed to decrypt: ' + prettyPrint(err))
       })
     })
   })
 })
 
-function toBytes(str) {
-  var ret = new Uint8Array(str.length)
-  for (var i = 0; i < ret.length; i++) {
-    if (str.charCodeAt(i) > 255) {
-      throw new Error('Expected ASCII')
-    }
-    ret[i] = str.charCodeAt(i)
+function prettyPrint (obj, level) {
+  if (typeof obj !== 'object' || obj === undefined || obj === null) {
+    return '' + obj
   }
+  if (obj instanceof ArrayBuffer) {
+    return new Buffer(obj).toString('hex')
+  }
+  if (Array.isArray(obj) && obj.length === 0) {
+    return '[]'
+  }
+  if (Object.keys(obj).length === 0) {
+    return '{}'
+  }
+
+  var nextLevel = (level || 0) + 1
+  var newline = '\n' + new Array(nextLevel).join('  ')
+  // works for both objects and arrays
+  var lines = Object.keys(obj).map(function (key) {
+    return key + ': ' + prettyPrint(obj[key], nextLevel)
+  })
+  return newline + lines.join(newline)
+}
+
+function toBytes (str) {
+  return new Uint8Array(new Buffer(str, 'utf8'))
 }
